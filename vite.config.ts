0dd2333a1b9b2a342000add004d71088aafae4e3
jerry george <jerrygithub360@gmail.com>
2026-06-24 17:47:09 +0100

    fix(config files):fixed stale vite file

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    allowedHosts: [
+      "d53e-2605-59c0-e97-4e10-735f-5c0d-9b6d-4798.ngrok-free.app",
+    ],
+  },
 });
